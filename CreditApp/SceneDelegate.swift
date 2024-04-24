3c06588819715190e3fbd0a2075563d82597f8f4
sulkhayeva <163290453+sulkhayeva@users.noreply.github.com>
2024-04-24 20:07:17 +0400

    finished

--- CreditApp/SceneDelegate.swift
+++ CreditApp/SceneDelegate.swift
@@ -13,10 +13,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create a new UIWindow with the given scene
+        let newWindow = UIWindow(windowScene: windowScene)
+        
+        // Create your new root view controller
+        let vc = UIStoryboard.init(name: "Login", bundle: Bundle.main).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController ?? LoginViewController()
+        // Set the new root view controller
+        let navigationController = UINavigationController(rootViewController: vc)
+        newWindow.rootViewController = navigationController
+        
+        
+//        if UserDefaultsHelper.getBool(key: Constant.UD_IS_LOGIN_KEY) {
+//            let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "QuestionsViewController") as? QuestionsViewController ?? QuestionsViewController()
+//            // Set the new root view controller
+//            let navigationController = UINavigationController(rootViewController: vc)
+////            navigationController.popToRootViewController(animated: true)
+//            newWindow.rootViewController = navigationController
+//        } else {
+//            let vc = UIStoryboard.init(name: "Auth", bundle: Bundle.main).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController ?? LoginViewController()
+//            let navigationController = UINavigationController(rootViewController: vc)
+//            // Set the new root view controller
+//            newWindow.rootViewController = navigationController
+//        }
+        
+        
+        
+        
+        // Make the window visible
+        window = newWindow
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
