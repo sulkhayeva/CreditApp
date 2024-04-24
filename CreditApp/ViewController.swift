b6ac8aa465a359f7b1667e824de6125b5186ed12
sulkhayeva <163290453+sulkhayeva@users.noreply.github.com>
2024-04-24 18:56:04 +0400

    Initial commit

---
+++ CreditApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreditApp
+//
+//  Created by Gumru on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
