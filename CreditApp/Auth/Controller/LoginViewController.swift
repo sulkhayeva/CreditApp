c62e3f634de154159e01f76601616813ffcb9877
sulkhayeva <163290453+sulkhayeva@users.noreply.github.com>
2024-04-27 21:47:22 +0400

    Login and Register UI

--- CreditApp/Auth/Controller/LoginViewController.swift
+++ CreditApp/Auth/Controller/LoginViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet private weak var usernameField: UITextField!
+    @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var signIn: UIButton!
+    @IBOutlet private weak var signUp: UIButton!
 
+    @IBAction func signInAction(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func signUpAction(_ sender: UIButton){
+        signUpButtonClicked()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDesign()
         // Do any additional setup after loading the view.
     }
+    
+    fileprivate func configureDesign(){
+        let redValue: CGFloat = 178.0 / 255.0
+        let greenValue: CGFloat = 89.0 / 255.0
+        let blueValue: CGFloat = 45.0 / 255.0
+        let borderColorField = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+        
+        usernameField.layer.borderWidth = 1.0
+        usernameField.layer.borderColor = borderColorField.cgColor
+        usernameField.layer.cornerRadius = 6
+        passwordField.layer.borderWidth = 1.0
+        passwordField.layer.borderColor = borderColorField.cgColor
+        passwordField.layer.cornerRadius = 6
+        
+        let redColor: CGFloat = 32.0 / 255.0
+        let greenColor: CGFloat = 49.0 / 255.0
+        let blueColor: CGFloat = 96.0 / 255.0
+        let borderColorButton = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
+        
+        signIn.layer.cornerRadius = 6
+        signUp.layer.cornerRadius = 6
+        signUp.layer.borderWidth = 1.0
+        signUp.layer.borderColor = borderColorButton.cgColor
+        
+    }
 
+    fileprivate func signUpButtonClicked(){
+        let vc = UIStoryboard.init(name: "Login", bundle: Bundle.main).instantiateViewController(withIdentifier: "RegisterController") as? RegisterController ?? RegisterController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
--- CreditApp/Auth/Controller/RegisterController/RegisterController.swift
+++ CreditApp/Auth/Controller/RegisterController/RegisterController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class RegisterController: UIViewController {
+    
+    @IBOutlet private weak var nameField: UITextField!
+    @IBOutlet private weak var surnameField: UITextField!
+    @IBOutlet private weak var fathersName: UITextField!
+    @IBOutlet private weak var idenfication: UITextField!
+    @IBOutlet private weak var usernameTextfield: UITextField!
+    @IBOutlet private weak var passwordTextfield: UITextField!
+    @IBOutlet private weak var signInButton: UIButton!
+    @IBOutlet private weak var backButton: UIButton!
+    
+    @IBAction func signInActionRegister(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func backAction(_ sender: UIButton){
+        navigationController?.popViewController(animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDesign()
+        navigationController?
+            .setNavigationBarHidden(true, animated: true) //default backi gorunmez edir
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func configureDesign(){
+        let redColor: CGFloat = 32.0 / 255.0
+        let greenColor: CGFloat = 49.0 / 255.0
+        let blueColor: CGFloat = 96.0 / 255.0
+        let borderColorButton = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1.0)
+        
+        nameField.layer.cornerRadius = 6
+        nameField.layer.borderWidth = 1.0
+        nameField.layer.borderColor = borderColorButton.cgColor
+        
+        surnameField.layer.cornerRadius = 6
+        surnameField.layer.borderWidth = 1.0
+        surnameField.layer.borderColor = borderColorButton.cgColor
+        
+        fathersName.layer.cornerRadius = 6
+        fathersName.layer.borderWidth = 1.0
+        fathersName.layer.borderColor = borderColorButton.cgColor
+        
+        idenfication.layer.cornerRadius = 6
+        idenfication.layer.borderWidth = 1.0
+        idenfication.layer.borderColor = borderColorButton.cgColor
+        
+        usernameTextfield.layer.cornerRadius = 6
+        usernameTextfield.layer.borderWidth = 1.0
+        usernameTextfield.layer.borderColor = borderColorButton.cgColor
+        
+        passwordTextfield.layer.cornerRadius = 6
+        passwordTextfield.layer.borderWidth = 1.0
+        passwordTextfield.layer.borderColor = borderColorButton.cgColor
+        
+        signInButton.layer.cornerRadius = 6
     }
-    */
 
 }
