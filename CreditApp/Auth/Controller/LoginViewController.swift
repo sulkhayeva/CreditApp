5877354f55f70eaa2a00c518ca64377faa26972a
sulkhayeva <163290453+sulkhayeva@users.noreply.github.com>
2024-04-28 15:16:36 +0400

    Login textfield delegate

--- CreditApp/Auth/Controller/LoginViewController.swift
+++ CreditApp/Auth/Controller/LoginViewController.swift
@@ -14,7 +14,7 @@ class LoginViewController: UIViewController {
     @IBOutlet private weak var signUp: UIButton!
 
     @IBAction func signInAction(_ sender: UIButton){
-        
+        signInClicked()
     }
     
     @IBAction func signUpAction(_ sender: UIButton){
@@ -24,6 +24,8 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureDesign()
+        usernameField.delegate = self
+        passwordField.delegate = self
         // Do any additional setup after loading the view.
     }
     
@@ -41,11 +43,56 @@ class LoginViewController: UIViewController {
         signUp.layer.borderColor = UIColor(named: "signUpBorder")?.cgColor
         
     }
+    
+    fileprivate func signInClicked(){
+        guard let username = usernameField.text,
+              let pass = passwordField.text else {return}
+        if username.count > 0 && username.count < 7 || pass.count > 0 && pass.count < 7 {
+            showMessage("Username və password simvol sayı 7 olmalıdır!")
+        } 
+        
+        else if username.count == 0 || pass.count == 0 {
+            showMessage("Melumatları daxil edin!")
+        }
+        
+        else {
+            checkUser(username: username, pass: pass)
+        }
+    }
+    
+    fileprivate func checkUser(username: String, pass: String){
+       
+    }
 
     fileprivate func signUpButtonClicked(){
         let vc = UIStoryboard.init(name: "Login", bundle: Bundle.main).instantiateViewController(withIdentifier: "RegisterController") as? RegisterController ?? RegisterController()
         navigationController?.pushViewController(vc, animated: true)
     }
 
+}
+
+extension LoginViewController: UITextFieldDelegate{
+        
+        func showMessage(_ message: String) {
+            let alertController = UIAlertController(title: message, message: "", preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            alertController.view.tintColor = UIColor.systemIndigo
+            present(alertController, animated: true, completion: nil)
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            guard let text = textField.text else {return}
+            switch textField {
+            case usernameField:
+                usernameField.layer.borderColor = text.count > 7 ? UIColor(named: "usernameBorder")?.cgColor : UIColor.red.cgColor
+                usernameField.layer.borderWidth = text.count > 7 ? 1.0 : 2.0
+                break
+            case passwordField:
+                passwordField.layer.borderColor = text.count > 7 ? UIColor(named: "usernameBorder")?.cgColor : UIColor.red.cgColor
+                passwordField.layer.borderWidth = text.count > 7 ? 1.0 : 2.0
+                break
+            default: break
+            }
+        }
 }
 
